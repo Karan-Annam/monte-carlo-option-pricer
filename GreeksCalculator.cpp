cc28180be60af150d13c32f31327b1d319f3a5fa
Karan <karansenaa@gmail.com>
2025-07-23 08:51:15 -0700

    modified

--- GreeksCalculator.cpp
+++ GreeksCalculator.cpp
@@ -16,15 +16,13 @@ double greeks::calculate_delta(Option& option, MonteCarloOptionPricer& pricer, d
 
     double temp = inputs.spot_price;
 
-    for(int i = 0; i < num_bumps; i++){
-        inputs.spot_price = inputs.spot_price + i * bump_size;
-        double up_price = pricer.price_option(option, inputs, 100, 1000000);
-        inputs.spot_price = temp;
+    for(int i = 1; i < num_bumps; i++){
+        inputs.spot_price = temp + i * bump_size;
+        double up_price = pricer.price_option(option, inputs, 100, 100000);
 
-        inputs.spot_price = inputs.spot_price - i * bump_size;
-        double down_price = pricer.price_option(option, inputs, 100, 1000000);
-        sum_finite_diff += (up_price - down_price) / (2 * bump_size);
-        inputs.spot_price = temp;
+        inputs.spot_price = temp - i * bump_size;
+        double down_price = pricer.price_option(option, inputs, 100, 100000);
+        sum_finite_diff += (up_price - down_price) / (2 * i * bump_size);
 
         std::cout <<"path " << i << " completed" << std::endl;
     }
@@ -41,15 +39,16 @@ double greeks::calculate_gamma(Option& option, MonteCarloOptionPricer& pricer, d
 
     int num_bumps = 10;
     int bump_size = 0.01;
+    double temp = inputs.spot_price;
 
     double sum_finite_diff = 0;
 
     for(int i = 0; i < num_bumps; i++){
-        inputs.spot_price = calc_price + i * bump_size;
+        inputs.spot_price = temp + i * bump_size;
         double up_price = pricer.price_option(option, inputs, 100, 10000);
-        inputs.spot_price = calc_price - i * bump_size;
+        inputs.spot_price = temp - i * bump_size;
         double down_price = pricer.price_option(option, inputs, 100, 10000);
-        sum_finite_diff += (up_price - 2 * calc_price + down_price) / (bump_size * bump_size);
+        sum_finite_diff += (up_price - 2 * calc_price + down_price) / (bump_size * i * bump_size);
     }
 
     return sum_finite_diff / num_bumps;
